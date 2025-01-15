45b51e4b82a82e61b36a8d820caafc0ef894fe30
Ігор Гавриленко <igor.gavrylenko05@gmail.com>
2025-01-15 23:51:24 +0100

    Updated Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,19 @@
+import {useState} from 'react';
+
 export default function Player({name, symbol}) {
+    const [ isEditing, setIsEditing ] = useState(false);
+    const [ newName, setNewName ] = useState(name);
+
     return (
             <li>
-                <span>
-                  <span className='player-name'>{ name }</span>
-                  <span className="player-symbol">{ symbol }</span>
+                <span className={"player"}>
+                    {isEditing ? <input type="text" defaultValue={newName} value={newName} onChange={(e) => setNewName(e.target.value)}/> :
+                        <span className={!isEditing ? "player-name" : ""}>{newName}</span>}
+                    <span className="player-symbol">{symbol}</span>
                 </span>
-                <button>Edit</button>
+                <button onClick={() => {
+                    setIsEditing(wasEditing => !wasEditing);
+                }}>{isEditing ? "Save" : "Edit"}</button>
             </li>
     );
 }
